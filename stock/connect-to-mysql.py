8d0a3aea8beaa3355f08bdae23519c5f9c31e1fc
Xpire <xpire0215@gmail.com>
2020-02-08 15:40:14 +0800

    Insert successfully

--- stock/connect-to-mysql.py
+++ stock/connect-to-mysql.py
@@ -1,13 +1,39 @@
 import pymysql
 
 def main():
-    conn = pymysql.connect(host='localhost', user='pystock', password='pystock', db='pystock')
-    cur = conn.cursor()
-    cur.execute("SHOW TABLES")
-    for r in cur:
-        print(r)
-    cur.close()
-    conn.close()
+    connection = pymysql.connect(
+            host='localhost', 
+            user='pystock', 
+            password='pystock', 
+            db='pystock',
+            charset='utf8'
+    )
+    
+    company_name = '\'台積電\''
+
+    # INSERT Method
+    try:
+        with connection.cursor() as cursor:
+            sql = "INSERT INTO `interest_list` (`code`, `name`) VALUES (2330, {})".format(company_name)
+            cursor.execute(sql)
+            connection.commit()
+    except:
+        print("Insert table failure")
+
+'''
+    # QUERY Method    
+    try:
+        with connection.cursor() as cursor:
+            sql = "SHOW TABLES"
+            cursor.execute(sql)
+            result = cursor.fetchone()
+            print(result)
+            cursor.close()              
+    except:
+        print("Execute SQL Failure")
+    
+    connection.close()
+'''
 
 if __name__ == '__main__':
     main()
